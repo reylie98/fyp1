519a01a3dcc61922ec017c94e5152ee527b7cd29
reylie98 <reynaldilie1@gmail.com>
2019-05-17 14:55:47 +0800

    make dinamic interface
    display all product from database to front end

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Product;
 
 class IndexController extends Controller
 {
     public function index(){
-        return view('index');
+        $allProducts = Product::get();
+        return view('index')->with(compact('allProducts'));
     }
 }
