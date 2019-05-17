9dc4ca6120ca86a94ecc1fea263866cd06bd7d51
reylie98 <reynaldilie1@gmail.com>
2019-05-17 15:17:55 +0800

    logic for call the categories
    using foreach loop

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,11 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Product;
+use App\Category;
 
 class IndexController extends Controller
 {
     public function index(){
-        $allProducts = Product::get();
+        $allProducts = Product::orderBy('id','DESC')->get();
+        
+        $categories = Category::where(['parent_id'=>0])->get();
+        // $categories = json_decode(json_encode($categories));
+        foreach($categories as $cat){
+            echo $cat->name;
+            $sub_categories = Category::where(['parent_id'=>$cat->id])->get();
+            foreach($sub_categories as $subcat){
+                echo $subcat->name;
+            }
+        }
+        die;
         return view('index')->with(compact('allProducts'));
     }
 }
