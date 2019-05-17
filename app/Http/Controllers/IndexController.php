93538a0c1e25045e74fb2dc387dfc76a273f7bcf
reylie98 <reynaldilie1@gmail.com>
2019-05-17 16:16:36 +0800

    fix the category display
    using relationship in category has many parent id to diplay the sub category

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -11,16 +11,9 @@ class IndexController extends Controller
     public function index(){
         $allProducts = Product::orderBy('id','DESC')->get();
         
-        $categories = Category::where(['parent_id'=>0])->get();
-        // $categories = json_decode(json_encode($categories));
-        foreach($categories as $cat){
-            echo $cat->name;
-            $sub_categories = Category::where(['parent_id'=>$cat->id])->get();
-            foreach($sub_categories as $subcat){
-                echo $subcat->name;
-            }
-        }
-        die;
-        return view('index')->with(compact('allProducts'));
+        $categories = Category::with('categories')->where(['parent_id'=>0])->get();
+        //$categories = json_decode(json_encode($categories));
+        
+        return view('index')->with(compact('allProducts','categories'));
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -73,25 +73,25 @@
 						<h2>Category</h2>
 						<div class="panel-group category-products" id="accordian"><!--category-productsr-->
 							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title">
-										<a data-toggle="collapse" data-parent="#accordian" href="#sportswear">
-											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
-											Sportswear
-										</a>
-									</h4>
-								</div>
-								<div id="sportswear" class="panel-collapse collapse">
-									<div class="panel-body">
-										<ul>
-											<li><a href="#">Nike </a></li>
-											<li><a href="#">Under Armour </a></li>
-											<li><a href="#">Adidas </a></li>
-											<li><a href="#">Puma</a></li>
-											<li><a href="#">ASICS </a></li>
-										</ul>
+								@foreach($categories as $cat)
+									<div class="panel-heading">
+										<h4 class="panel-title">
+											<a data-toggle="collapse" data-parent="#accordian" href="#{{$cat->id}}">
+												<span class="badge pull-right"><i class="fa fa-plus"></i></span>
+												{{$cat->name}}
+											</a>
+										</h4>
 									</div>
-								</div>
+									<div id="{{$cat->id}}" class="panel-collapse collapse">
+										<div class="panel-body">
+											<ul>
+												@foreach($cat->categories as $subcat)
+													<li><a href="#">{{ $subcat->name }} </a></li>
+												@endforeach
+											</ul>
+										</div>
+									</div>
+								@endforeach
 							</div>
 							<div class="panel panel-default">
 								<div class="panel-heading">
