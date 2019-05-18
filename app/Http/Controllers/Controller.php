f4f66b1e06b7bfdbaca11179bec565acc5b0c7d9
reylie98 <reynaldilie1@gmail.com>
2019-05-18 13:15:36 +0800

    fixing header for shop
    using foreach loop in controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,13 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+use App\Category;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public static function maincategories(){
+        $mainCategories = Category::where(['parent_id'=>0])->get();
+        return $mainCategories;
+    }
 }
